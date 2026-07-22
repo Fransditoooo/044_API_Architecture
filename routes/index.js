9a7ba4f502ae408f061c5ebc3686baf8aadac3bb
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-22 15:08:03 +0700

    inisialisasi konfigurasi sequelize orm dan environment

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'API Architecture Meeting 6 is running'
+  });
+});
+
+module.exports = router;
