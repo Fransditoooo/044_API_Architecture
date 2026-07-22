5a8f615336a119816d226f2a511b395eb2f38a2f
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-07-22 15:10:44 +0700

    konfigurasi instansiasi sequelize dan pemuatan otomatis model

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require("express");
-const connectDatabase = require("./config/db");
+require('dotenv').config();
+const express = require('express');
+const connectDatabase = require('./config/db');
+const routes = require('./routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Rute Utama (Agar saat buka http://localhost:3000 langsung muncul dan TIDAK Cannot GET)
+app.get('/', (req, res) => {
+  res.send('API Server is running...');
+});
+
+// Rute API (/api)
+app.use('/api', routes);
+
 async function startServer() {
-    await connectDatabase();
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+  await connectDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
 }
 
 startServer();
